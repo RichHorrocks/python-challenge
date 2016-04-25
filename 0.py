2e70b2c69a4fbb8af455fbc0d46e17730934f144
Richard Horrocks <richardjhorrocks@gmail.com>
2016-04-25 09:20:25 +0100

    First commit.

---
+++ 0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# http://www.pythonchallenge.com/pc/def/0.html
+
+# Just an exercise in using the 'power of' opperation, '**'
+# Note that from Python 3, print() is a function, rather than a statement.
+# It's therefore necessary to include brackets (braces) when calling.
+print(2 ** 38)
